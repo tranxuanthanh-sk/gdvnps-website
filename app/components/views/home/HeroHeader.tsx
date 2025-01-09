338580ef4d32f033de78d980d956e656e6752e5d
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2025-01-09 12:19:44 +0700

    fix something

---
+++ app/components/views/home/HeroHeader.tsx
@@ -0,0 +1,12 @@
+export default function HeroHeader() {
+  return (
+    <div className="font-sans">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold leading-4">
+        Chào mừng đã đến với
+      </h1>
+      <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold leading-4">
+        GDVNPS
+      </h1>
+    </div>
+  );
+}
---
+++ app/components/views/home/index.ts
@@ -0,0 +1,3 @@
+import HeroHeader from "./HeroHeader";
+
+export { HeroHeader };
