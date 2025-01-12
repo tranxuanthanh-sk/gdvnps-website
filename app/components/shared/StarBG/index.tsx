cc9c8665ab6b82c1f0ad884eba0ae78902e288df
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2025-01-12 09:26:05 +0700

    chore: fix build command exits with 1 issues
    tailwind.css and vercel.json were not pushed, i do not know why.

---
+++ app/components/shared/StarBG/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./star-bg.module.css";
+
+export function StarBG({ ...props }) {
+  return (
+    <div className={styles["star-bg"]} aria-hidden="true" {...props}>
+      {Array.from({ length: 20 }, (_, i) => (
+        <div key={i} className={styles["star-bg__obj"]}></div>
+      ))}
+    </div>
+  );
+}
