aefa4423a452434a7317c58b0e5c8204061ed839
tranxuanthanh-sk <tranxuanthanhreal@gmail.com>
2025-01-15 21:44:03 +0700

    upd

--- app/components/views/home/HeroHeader.tsx
+++ app/components/views/home/HeroHeader.tsx
@@ -1,29 +1,16 @@
-type GDVNPSCharType = {
-  id: number;
-  char: string;
-}[];
+import clsx from "clsx";
+import styles from "./HeroHeader.module.scss"
 
 export default function HeroHeader() {
-  const GDVNPSChar: GDVNPSCharType = [
-    { id: 1, char: "G" },
-    { id: 2, char: "D" },
-    { id: 3, char: "V" },
-    { id: 4, char: "N" },
-    { id: 5, char: "P" },
-    { id: 6, char: "S" },
-  ];
-
   return (
-    <div className="font-sans">
+    <div className={clsx(styles['font-hero'], "font-sans text-center px-4 mx-6 lg:mx-36")}>
       <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold">
         Chào mừng đã đến với
       </h1>
       <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold mt-0.5">
-        {/* GDVNPS */}
-        {GDVNPSChar.map((textChar) => (
-          <span key={textChar.id}>{textChar.char}</span>
-        ))}
+        GDVNPS
       </h1>
+      <p style={{wordWrap: "break-word"}} className="text-slate-300/90 py-6 px-20 text-base lg:text-lg tracking-wide font-normal leading-[1.85]">Lập ra vào 2024, GDVNPS là máy chủ Geometry Dash Private Server dành cho <strong>cộng đồng người chơi Geometry Dash tại Việt Nam</strong></p>
     </div>
   );
 }
